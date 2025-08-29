4e097dfa88b899ac4b66964dd6943bb2e7a6e744
kimmanback1-creator <kimmanback1@gmail.com>
2025-08-30 01:04:49 +0900

    Remove unnecessary files

--- auto_reload.py
+++
@@ -1,8 +0,0 @@
-import os
-from watchgod import run_process
-
-def run_server():
-    os.system("py TradingServer.py")  # 서버 실행 명령
-
-if __name__ == "__main__":
-    run_process('.', run_server)  # 현재 폴더 안 .py 파일 변경 감시
