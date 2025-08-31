7166002e84af7c905895bb0b971a4e70ffdf149b
ds1 <kimmanback1@gmail.com>
2025-08-31 15:48:24 +0900

    Update TradingServer.py

--- TradingServer.py
+++ TradingServer.py
@@ -678,7 +678,7 @@ async def on_startup():
 
     job_queue.run_monthly(
     lambda ctx: send_report(telegram_app.bot, period="month"),
-    time=time(hour=22, minute=0),
+    when=time(hour=22, minute=0),
     day=1
 )
 
@@ -697,6 +697,7 @@ async def webhook(request: Request):
     except Exception as e:
         print("❌ Webhook error:", e)
         return JSONResponse(content={"ok": False, "error": str(e)}, status_code=500)
+
 
 
 
